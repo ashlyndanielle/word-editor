ad490fe07edbbaa0de1b62fc9a98f42960812bfe
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-10-18 11:13:46 -0600

    finished project and solved some of the issues

--- src/App.js
+++ src/App.js
@@ -9,26 +9,59 @@ import TextContainer from './components/TextContainer';
 
 class App extends Component {
   // constructor
-
+  constructor(){
+    super();
+    this.state = {
+      fontColor: 'Black',
+      fontSize: 12,
+      fontFamily: 'Monospace',
+      allowEdit: 'true'
+    }
+  }
   // updateColor
-
+  updateColor = val => {
+    this.setState({
+      fontColor: val
+    })
+  }
   // updateSize
-
+  updateSize = val => {
+    this.setState({
+      fontSize: val
+    })
+  }
   // updateFamily
-
+  updateFamily = val => {
+    this.setState({
+      fontFamily: val
+    })
+  }
   // updateEditStatus
-
+  updateEditStatus = val => {
+    this.setState({
+      allowEdit: val
+    })
+  }
   render() {
+    const styles = {
+      color: this.state.fontColor,
+      fontSize: this.state.fontSize,
+      fontFamily: this.state.fontFamily
+    }
     return (
       <div>
         <div className="headerBar">
-          { /* Render EditToggle */ }
-          { /* Render ColorChanger */ }
-          { /* Render SizeChanger */ }
-          { /* Render FamilyChanger */ }
+          <EditToggle updateEditStatus={ this.updateEditStatus } />
+          <ColorChanger updateColor={ this.updateColor } allowEdit={ this.state.allowEdit }/>
+          <SizeChanger updateSize={ this.updateSize } allowEdit={ this.state.allowEdit }/>
+          <FamilyChanger updateFamily={ this.updateFamiy } allowEdit={ this.state.allowEdit }/>
         </div>
         <div className="textArea">
-          { /* Render TextContainer */ }
+          <TextContainer
+            color={ this.state.fontColor }
+            fontFamily={ this.state.fontFamily }
+            fontSize={ this.state.fontSize }
+           />
         </div>
       </div>
     )
--- src/components/FamilyChanger.js
+++ src/components/FamilyChanger.js
@@ -12,10 +12,10 @@ export default class FamilyChanger extends Component {
 
   render() {
     return (
-      <select className="dropDownContainer">
-        <option value="monospace"> Monospace </option>
-        <option value="arial"> Arial </option>
-        <option value="courier"> Courier </option>
+      <select className="dropDownContainer" onChange={ e => this.props.updateFamily(e.target.value) } disabled={ this.props.allowEdit === 'false' } >
+        <option value="Monospace"> Monospace </option>
+        <option value="Arial"> Arial </option>
+        <option value="Courier"> Courier </option>
       </select>
     )
   }
